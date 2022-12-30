a5e1806a5af6e8a30244a1a0d6d43cf487fc7299
Franco Perez <franco.perez03@gmail.com>
2022-12-29 23:07:04 -0300

    setup project

---
+++ contracts/Raffle.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Raffle {
+
+}
+//Enter the lottery
+//Pick a random winner
+//Winner to be selected every x minutes -> completely automated
+
+//Chainlink Oracle -> Randomness, Automated Execution(CHainlink keepers)
