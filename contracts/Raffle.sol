bd5229d894fc2760c42b2be49bac16fc4d5db259
Franco Perez <franco.perez03@gmail.com>
2022-12-30 16:39:27 -0300

    created Raffle contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,8 +1,35 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Raffle {
+error Raffle_NotEnoughETHEntered();
 
+contract Raffle {
+    /*State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) revert Raffle_NotEnoughETHEntered();
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    function pickRandomWinner() public {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
 }
 //Enter the lottery
 //Pick a random winner
