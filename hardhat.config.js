7a17a1645ff2b88cbf6caab2b554e9e2102dde49
Franco Perez <franco.perez03@gmail.com>
2023-01-04 19:26:32 -0300

    created vrfCoordinator mocks

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,34 @@ require('solidity-coverage')
 require('hardhat-gas-reporter')
 require('hardhat-contract-sizer')
 require('dotenv').config()
-/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || 'https://eth-mainnet.alchemyapi.io/v2/your-api-key'
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY || '0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ''
+
 module.exports = {
     solidity: '0.8.17',
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 1,
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
 }
